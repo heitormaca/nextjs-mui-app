bcd1d46a49fb6032093a350ccfb4b1acaf03b6a6
heitorsoares <heitor_soares@live.com>
2024-03-26 19:55:08 -0300

    persist color mode

--- src/core/context/ColorModeContext/ColorModeContext.provider.tsx
+++ src/core/context/ColorModeContext/ColorModeContext.provider.tsx
@@ -1,6 +1,5 @@
 'use client'
-
-import { createContext, useContext, useState } from 'react'
+import { createContext, useContext, useEffect, useState } from 'react'
 import {
   ColorMode,
   ColorModeContextParams,
@@ -13,17 +12,25 @@ const ColorModeContext = createContext({} as ColorModeContextParams)
 export function ColorModeProvider(props: ColorModeProviderProps) {
   const [colorMode, setColorMode] = useState<ColorMode>('light')
 
-  function toggleColorMode() {
-    setColorMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'))
-  }
-
   function darkMode() {
     setColorMode('dark')
+    localStorage.setItem('colorMode', 'dark')
   }
 
   function lightMode() {
     setColorMode('light')
+    localStorage.setItem('colorMode', 'light')
   }
+
+  const cm = localStorage.getItem('colorMode')
+
+  useEffect(() => {
+    if (!cm) {
+      localStorage.setItem('colorMode', colorMode)
+    } else {
+      setColorMode(cm as ColorMode)
+    }
+  }, [colorMode, cm])
 
   return (
     <ColorModeContext.Provider value={{ colorMode, lightMode, darkMode }}>
